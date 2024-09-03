9230e00a1f8add5781744bfb90f466be64189dd7
JJJinWg <162809387+JJJinWg@users.noreply.github.com>
2024-09-03 10:05:12 +0900

    day2

---
+++ day01/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 300000000;
+        byte b1 = (byte)200 ;
+        System.out.println(b1);
+
+        byte num1 = 100;
+        byte num2 = 100;
+        byte num3 = (byte) (num1 + num2);
+        System.out.println(num3);
+
+        long num4 = 3000000000L;
+        long num5 = 1500000000L + 1500000000L;
+        System.out.println(num5);
+
+        int i1 = 1500000000;
+        int i2 = 1500000000;
+        long result = i1*1L + i2*1L;
+        System.out.println(result);
+    }
+}
